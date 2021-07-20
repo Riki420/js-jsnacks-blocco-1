a69448678449099bacf860ac13b7bbb05ea6e649
Riki420 <81353585+Riki420@users.noreply.github.com>
2021-07-20 13:32:59 +0200

    add Object Snack

---
+++ Snack Oggetti JS/js/script.js
@@ -0,0 +1,58 @@
+var palla = {                                               //creo un oggetto
+    nome: 'palla',                                          
+    peso: 10,
+}
+var display = document.getElementById('snack1');            //elemento per il display
+
+printObject(palla, display);                                //faccio partire la funzione
+
+
+// var userWeight = parseInt(prompt('cambia il peso'));        //chiedo all'utente di cambiarne il peso
+
+
+//--------------------------------------------------------------------------------------//
+//                                   //*INPUT UTENTE                                    //
+//--------------------------------------------------------------------------------------//
+
+
+var weightField = document.getElementById('weight-num');
+var nameField = document.getElementById('name');
+var button = document.getElementById('btn');
+
+button.addEventListener('click', function () {
+    
+    //recupero dei valori
+    var weightValue = weightField.value;
+    console.log(weightValue);
+    palla.peso = weightValue;
+    printObject(palla, display); 
+});
+
+
+//****************************************************************
+
+
+
+
+// palla.peso = userWeight;                                    //cambio la chiave dell'oggetto con il peso dell'utente
+
+printObject(palla, display);                                //faccio partire la funzione
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------//
+//                                DICHIARAZIONI FUNZIONI                                //
+//--------------------------------------------------------------------------------------//
+
+
+function printObject(obj, targetElement){
+    var content = '';                                                            //creo una stringa vuota
+    for(var key in obj){                                                        //giro dentro l'oggetto
+    content += '<strong>' + key + ':</strong> ' + obj[key] + '<br>';           //concateno content con le chiavi dell'oggetto
+}
+    targetElement.innerHTML = content;                                       //stampo sul display il content
+
+}
---
+++ Snack Oggetti JS/js/script2.js
@@ -0,0 +1,81 @@
+// ┌──────────────────────────────────────────────────────────────────────────────┐
+// │ SNACK: Creiamo un array di oggetti contenente 3 squadre di calcio con        │
+// │ proprietà "nome", "città"                                                    │
+// │ "punti";                                                                     │
+// │ Stampiamo le squadre in pagina, all'interno di una tabella HTML              │
+// └──────────────────────────────────────────────────────────────────────────────┘
+
+
+var teamList = [
+    {
+        team: 'As Roma',
+        citta: 'Roma',
+        punti: 69
+    },
+    {
+        team: 'Pescara FC',
+        citta: 'Pescara',
+        punti: 32
+    },
+    {
+        team: 'Juventus',
+        citta: 'Torino',
+        punti: 60
+    },
+    {
+        team: 'Milan',
+        citta: 'Milano',
+        punti: 40
+    },
+    {
+        team: 'Inter',
+        citta: 'Milano',
+        punti: 32
+    },
+    {
+        team: 'Lazio',
+        citta: 'Nomadi',
+        punti: 2
+    },
+    {
+        team: 'Bologna',
+        citta: 'Bologna',
+        punti: 25
+    },
+    {
+        team: 'Genoa',
+        citta: 'Genova',
+        punti: 12
+    },
+    {
+        team: 'Torino',
+        citta: 'Torino',
+        punti: 35
+    },
+    {
+        team: 'Napoli',
+        citta: 'Napoli',
+        punti: 10
+    }
+
+];
+
+teamList.sort(function(a,b){
+    return b.punti - a.punti;
+});
+
+var tableBody = document.getElementsByTagName('tbody')[0];
+var tableContent = '';
+
+for(var i = 0; i < teamList.length; i++){
+    var currentTeam = teamList[i];
+    tableContent += '<tr>'
+
+    tableContent += '<td>' + currentTeam.team + '</td>'
+    tableContent += '<td>' + currentTeam.punti + '</td>'
+    tableContent += '<td>' + currentTeam.citta + '</td>'
+
+    tableContent += '</tr>'
+}
+
+tableBody.innerHTML = tableContent;
